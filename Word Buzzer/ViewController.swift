f70e01d99a4a1ca9f0719306e28c476253549706
Abubakr Dar <mbkr1992@gmail.com>
2018-04-07 22:26:41 +0200

    Initial Commit

---
+++ Word Buzzer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word Buzzer
+//
+//  Created by Abubakr Dar on 4/7/18.
+//  Copyright © 2018 Abubakr Dar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
